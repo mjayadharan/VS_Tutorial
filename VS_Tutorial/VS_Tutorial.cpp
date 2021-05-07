e96ab58bf34c72da2ba7ef2ff1ef4e17511dfb45
mjayadharan <35903705+mjayadharan@users.noreply.github.com>
2021-05-07 14:01:42 -0500

    Add project files.

---
+++ VS_Tutorial/VS_Tutorial.cpp
@@ -0,0 +1,54 @@
+﻿// Tutorials.cpp : Defines the entry point for the application.
+//
+
+#include "VS_Tutorial.h"
+#include<vector>
+
+using namespace std;
+using namespace sample_name_space;
+
+template<class T>
+VectorDoubler<T>::VectorDoubler(vector<T> input_vect):
+	vector_copy(input_vect)
+{
+}
+
+template<class T>
+void VectorDoubler<T>::call(vector<T>& output_vector, int times)
+{
+	output_vector.resize(vector_copy.size());
+	for (int i = 0; i < output_vector.size(); i++)
+	{
+		output_vector[i] = vector_copy[i] * pow(2, times);
+
+	}
+	
+}
+
+template<class T>
+void VectorDoubler<T>::pronounce()
+{
+	for (auto el : vector_copy)
+	{
+		cout << el << ", ";
+	}
+	cout << endl;
+}
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	vector<double> double_vect(10, 0.0);
+	cout << sin(2)<<endl;
+	vector<double> dummy_vector = { 1, 2, 3, 4, 5 };
+	VectorDoubler<double> vector_doubler(dummy_vector);
+	vector<double> doubled_vector;
+	vector_doubler.call(doubled_vector, 1);
+	for (auto el : doubled_vector)
+	{
+		cout << el << endl;
+	}
+
+	return 0;
+}
---
+++ VS_Tutorial/VS_Tutorial.h
@@ -0,0 +1,24 @@
+﻿// Tutorials.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+namespace sample_name_space
+{
+	template<class T>
+	class VectorDoubler
+	{
+	public:
+		VectorDoubler<T>(vector<T> input_vect);
+		void call(vector<T>& output_vector, int times = 1);
+		void pronounce();
+	private:
+		vector<T> vector_copy;
+	};
+}
+// TODO: Reference additional headers your program requires here.
